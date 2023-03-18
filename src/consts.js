dfce1a9b64bfdbeeb0e04680a96eb8fe0861154c
Sweetrbis <68565627@qq,com>
2023-03-18 15:13:10 +0800

    change

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `Cyy的记录本`;
+export const SITE_TITLE = `Record Life`;
 export const SITE_DESCRIPTION = 'Caryon Record Life';
 export const SITE_EMAIL = 'czhcyyyy@gmail.com'
 export const SITE_NAME = 'nomore.cyynzn.top';
